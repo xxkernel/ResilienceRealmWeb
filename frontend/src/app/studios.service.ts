b0d603171382674ac5ffc6efcc3c7d9bd8fb2ce1
HiyoRitsu <122852098+HiyoRitsu@users.noreply.github.com>
2024-04-23 21:22:57 +0600

    added studios model

--- frontend/src/app/studios.service.ts
+++ frontend/src/app/studios.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Country } from './studios.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StudiosService {
-
-  constructor() { }
+  constructor(private client: HttpClient) {}
+  getAlmaty(): Observable<Country[]> {
+    return this.client.get<Country[]>(
+      'http://127.0.0.1:8000/studios/almaty/gym'
+    );
+  }
+  getAstana(): Observable<Country[]> {
+    return this.client.get<Country[]>(
+      'http://127.0.0.1:8000/studios/astana/gym'
+    );
+  }
+  getUralsk(): Observable<Country[]> {
+    return this.client.get<Country[]>(
+      'http://127.0.0.1:8000/studios/uralsk/gym'
+    );
+  }
+  getShymkent(): Observable<Country[]> {
+    return this.client.get<Country[]>(
+      'http://127.0.0.1:8000/studios/shymkent/gym'
+    );
+  }
+  getKaraganda(): Observable<Country[]> {
+    return this.client.get<Country[]>(
+      'http://127.0.0.1:8000/studios/karaganda/gym'
+    );
+  }
 }
